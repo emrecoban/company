946352393b8ae8d298b37e807c1e12642ade0907
emrecoban <fbdarktole@gmail.com>
2023-06-03 16:51:53 +0300

    Added social icons to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import style from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
         <div className={style.container}>
             <div>&copy; {new Date().getFullYear()} emreDev. All rights reserved.</div>
-            <div></div>
+            <div className={style.social}>
+                <Image className={style.icon} src="/1.png" width={15} height={15} alt="facebook" />
+                <Image className={style.icon} src="/2.png" width={15} height={15} alt="instagram" />
+                <Image className={style.icon} src="/3.png" width={15} height={15} alt="twitter" />
+                <Image className={style.icon} src="/4.png" width={15} height={15} alt="youtube" />
+            </div>
         </div>
     )
 }
