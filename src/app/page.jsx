c1abd1504377e1442503d26fa8743850738580fb
emrecoban <fbdarktole@gmail.com>
2023-06-03 17:17:59 +0300

    Designed home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
+import style from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={style.container}>
+      <div className={style.item}>
+        <h1 className={style.title}>Better design for your digital products.</h1>
+        <p className={style.desc}>Turning your ideas into app!</p>
+        <button className={style.button}>See Our Works</button>
+      </div>
+      <div className={style.item}>
+        <Image src={Hero} alt="" className={style.img} />
+      </div>
     </div>
   )
 }
