fa19110a84eb0d696b0dc7e6b94b102b790e5e40
emrecoban <fbdarktole@gmail.com>
2023-06-04 12:31:42 +0300

    Designed category page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from "./category.module.css"
+import Button from '@/components/button/Button'
+import Image from 'next/image'
 
-const Category = () => {
+
+const Category = ({ params }) => {
     return (
-        <div>Category</div>
+        <div className={styles.container}>
+            <h1 className={styles.catTitle}>{params.category}</h1>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Button text="See More" url="#" />
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image fill className={styles.img} src="" alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
 
