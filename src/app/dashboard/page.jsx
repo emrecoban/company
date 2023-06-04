64f5a7ba456609f2a6ff0640d6be240c24dc93ac
emrecoban <fbdarktole@gmail.com>
2023-06-04 21:48:25 +0300

    Installed SWR

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import style from './dashboard.module.css'
 
+
+
 const Dashboard = () => {
+    /*     const [data, setData] = useState([])
+        const [err, setErr] = useState(false)
+        const [isLoading, setIsLoading] = useState(false)
+    
+        useEffect(() => {
+            const getData = async () => {
+                setIsLoading(false)
+                const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    
+                if (!res.ok) {
+                    setErr(true)
+                }
+    
+                const data = await res.json()
+                setData(data)
+                setIsLoading(false)
+            }
+            getData()
+        }, []) */
+
     return (
         <div className={style.container}>Dashboard</div>
     )
