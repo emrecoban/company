1af21812983e7b439f94d74de3a7753a67e35341
emrecoban <fbdarktole@gmail.com>
2023-06-04 23:46:26 +0300

    Added dynamic metadata

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -12,6 +12,14 @@ async function getData(id) {
     return res.json();
 }
 
+export async function generateMetadata({ params }) {
+    const post = await getData(params.id)
+    return {
+        title: post.title,
+        description: post.desc,
+    };
+}
+
 const Post = async ({ params }) => {
     const data = await getData(params.id)
     return (
