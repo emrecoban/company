752cb59587bd7816b1f75af1da26ea3044c5934b
emrecoban <fbdarktole@gmail.com>
2023-06-05 14:51:34 +0300

    Designed login page

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import style from './login.module.css'
+'use client'
+
+import React, { useState } from 'react'
+import styles from './login.module.css'
+import Link from 'next/link';
 
 const Login = () => {
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+    }
     return (
-        <div className={style.container}>Login</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>{success ? success : "Welcome Back"}</h1>
+            <h2 className={styles.subtitle}>Please sign in to see the dashboard.</h2>
+
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    required
+                    className={styles.input}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    className={styles.input}
+                />
+                <button className={styles.button}>Login</button>
+                {error && error}
+            </form>
+            <button
+                onClick={() => {
+                    signIn("github");
+                }}
+                className={styles.button + " " + styles.google}
+            >
+                Login with Github
+            </button>
+            <span className={styles.or}>- OR -</span>
+            <Link className={styles.link} href="/dashboard/register">
+                Create new account
+            </Link>
+        </div>
     )
 }
 
