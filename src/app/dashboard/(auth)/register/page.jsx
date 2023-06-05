f4154fe1c750de0bed94a97164a9a8aa54ef82f2
emrecoban <fbdarktole@gmail.com>
2023-06-05 14:09:04 +0300

    Updated handleSubmit func.

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -2,14 +2,31 @@
 import React, { useState } from 'react'
 import styles from './register.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Register = () => {
     const [error, setError] = useState(null);
 
+    const router = useRouter()
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const name = e.target[0].value
+        const email = e.target[1].value
+        const password = e.target[2].value
 
-    const handleSubmit = () => {
-
+        try {
+            const res = await fetch('/api/auth/register', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(name, email, password)
+            })
+            res.status === 201 && router.push("/dashboard/login?success=Account has been created")
+        } catch (error) {
+            setError(true)
+        }
     }
 
     return (
