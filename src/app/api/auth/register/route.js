a08bfb6ffe12e12dcd6098b47aaafdffdb2642d5
emrecoban <fbdarktole@gmail.com>
2023-06-05 14:17:08 +0300

    Created register endpoint

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,15 +1,24 @@
 import User from "@/models/User"
 import connect from "@/utils/db"
 import { NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
 
 export const POST = async (req) => {
     const { name, email, password } = await req.json()
     await connect()
 
-    const newUser = new User()
+    const hashedPassword = await bcrypt.hash(password, 5)
+    const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+    })
 
     try {
-
+        await newUser.save()
+        return new NextResponse("User has been created", {
+            status: 201,
+        })
     } catch (error) {
         return new NextResponse(error.message, {
             status: 500,
