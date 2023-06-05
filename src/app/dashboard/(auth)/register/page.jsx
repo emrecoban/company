55855006cffefea982e4e9e993741395c373525a
emrecoban <fbdarktole@gmail.com>
2023-06-05 14:02:48 +0300

    Designed register page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import style from './register.module.css'
+'use client'
+import React, { useState } from 'react'
+import styles from './register.module.css'
+import Link from 'next/link'
 
 const Register = () => {
+    const [error, setError] = useState(null);
+
+
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div className={style.container}>Register</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Create an Account</h1>
+            <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    required
+                    className={styles.input}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    required
+                    className={styles.input}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    className={styles.input}
+                />
+                <button className={styles.button}>Register</button>
+                {error && "Something went wrong!"}
+            </form>
+            <span className={styles.or}>- OR -</span>
+            <Link className={styles.link} href="/dashboard/login">
+                Login with an existing account
+            </Link>
+        </div>
     )
 }
 
