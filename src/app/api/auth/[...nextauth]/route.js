d9389907d4921d103a4805bb2d7868dc17f91440
emrecoban <fbdarktole@gmail.com>
2023-06-05 14:46:26 +0300

    Added loginWithEmail

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,38 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-
+import CredentialsProvider from "next-auth/providers/credentials"
+import connect from "@/utils/db"
+import User from "@/models/User"
+import bcrypt from "bcryptjs";
 const handler = NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+        }),
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            async authorize(credentials) {
+                await connect()
+                try {
+                    const user = User.findOne({
+                        email: credentials.email
+                    })
+                    if (user) {
+                        const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+                        if (isPasswordCorrect) {
+                            return user
+                        } else {
+                            throw new Error("Wrong Credentials!")
+                        }
+                    } else {
+                        throw new Error("User not found!")
+                    }
+                } catch (error) {
+                    throw new Error(error)
+                }
+            }
         }),
     ],
 })
