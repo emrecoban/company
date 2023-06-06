46bd5ae7c201875b1c6fe82d4ef59e9bfec174b6
emrecoban <fbdarktole@gmail.com>
2023-06-06 14:58:45 +0300

    Visualizer added

---
+++ src/app/visual/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Visualizer from 'next-route-visualizer'
+
+const page = () => {
+    return (
+        <Visualizer displayColocating />
+    )
+}
+
+export default page
